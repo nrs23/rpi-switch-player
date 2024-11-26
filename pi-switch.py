c1b4ef29e496cf62cd8efd811f20bcf2aa0b3a91
Nic <nrs23@sussex.ac.uk>
2024-11-26 17:20:00 +0000

    Button switch on GPIO 2 starts audio.wav

--- pi-switch.py
+++ pi-switch.py
@@ -1,41 +1,25 @@
-import pygame
-import RPi.GPIO as GPIO
+from gpiozero import Button
+import simpleaudio as sa
 import sys
+import time
 
-pygame.mixer.music.load('audio.mp3')
+button = Button(2)
 
-GPIO.setwarnings(False)  # Ignore warning for now
-GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
-# Set pin 10 to be an input pin and set initial value to be pulled low (off)
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+wave_obj = sa.WaveObject.from_wave_file("audio.wav")
+play_obj = wave_obj.play()
+play_obj.stop()
 
-switch_open_event = pygame.USEREVENT + 1
-switch_closed_event = pygame.USEREVENT + 2
-
-
-def switch_open_callback(channel):
-    pygame.event.post(switch_open_event)
-
-
-def switch_closed_callback(channel):
-    pygame.event.post(switch_closed_event)
-
-
-# Setup event on pin 10 falling edge
-GPIO.add_event_detect(10, GPIO.FALLING, callback=switch_open_callback)
-
-# Setup event on pin 10 rising edge
-GPIO.add_event_detect(10, GPIO.RISING, callback=switch_closed_callback)
-
-pygame.init()
+reset_stream = False
 while 1:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            GPIO.cleanup()  # Clean up
-            sys.exit()
-        elif event.type == switch_open_event:
-            print("Starting audio")
-            pygame.mixer.music.play()
-        elif event.type == switch_closed_event:
-            print("Stopping audio")
-            pygame.mixer.music.stop()
+    try:
+        if button.is_pressed:
+            if not play_obj.is_playing():
+                play_obj = wave_obj.play()
+        else:
+            if play_obj.is_playing():
+                play_obj.stop()
+
+            time.sleep(0.1)
+    except KeyboardInterrupt:
+        play_obj.stop()
+        sys.exit()
